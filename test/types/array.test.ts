d3f158512c69d2a4837b91514e15ae381b095e94
ERoydev <e.roydev@gmail.com>
2025-10-24 17:30:17 +0300

    add: test placeholders

--- test/types/array.test.ts
+++ test/types/array.test.ts
@@ -1,39 +1,5 @@
-import {
-    arrayTypeNode,
-    definedTypeNode,
-    numberTypeNode,
-    prefixedCountNode,
-    publicKeyTypeNode,
-} from '@codama/nodes';
-// import { getFromRenderMap } from '@codama/renderers-core';
-import { visit } from '@codama/visitors-core';
-import { test } from 'vitest';
+import { expect, test } from 'vitest';
 
-import { DEFAULT_NAME_TRANSFORMERS, GetRenderMapOptions, getRenderMapVisitor } from '../../src';
-// import { codeContains, codeDoesNotContains } from '../_setup';
-
-test('test exports short vecs', () => {
-    // Given an array using a shortU16 prefix.
-    const node = definedTypeNode({
-        name: 'myShortVec',
-        type: arrayTypeNode(publicKeyTypeNode(), prefixedCountNode(numberTypeNode('shortU16'))),
-    });
-
-    const options: GetRenderMapOptions = {
-        nameTransformers: DEFAULT_NAME_TRANSFORMERS,
-    };
-
-    // When we render the array.
-    const renderMap = visit(node, getRenderMapVisitor(options));
-    console.log(renderMap);
-    // Then we expect a short vec to be exported.
-    // codeContains(getFromRenderMap(renderMap, 'types/my_short_vec.rs'), [
-    //     /pub type MyShortVec = ShortVec<Pubkey>;/,
-    //     /use solana_pubkey::Pubkey/,
-    //     /use solana_short_vec::ShortVec/,
-    // ]);
-    // codeDoesNotContains(getFromRenderMap(renderMap, 'types/my_short_vec.rs'), [
-    //     /use borsh::BorshSerialize/,
-    //     /use borsh::BorshDeserialize/,
-    // ]);
+test('it exports u8 numbers', () => {
+    expect(true).toBe(true);
 });
--- test/types/number.test.ts
+++ test/types/number.test.ts
@@ -1,33 +1,7 @@
-import { arrayTypeNode, definedTypeNode, numberTypeNode, prefixedCountNode, publicKeyTypeNode } from '@codama/nodes';
 // import { getFromRenderMap } from '@codama/renderers-core';
-import { visit } from '@codama/visitors-core';
-import { test } from 'vitest';
-
-import { DEFAULT_NAME_TRANSFORMERS, GetRenderMapOptions, getRenderMapVisitor } from '../../src';
+import { expect, test } from 'vitest';
 // import { codeContains, codeDoesNotContains } from '../_setup';
 
-test('it exports short vecs', () => {
-    // Given a shortU16 number.
-    const node = definedTypeNode({
-        name: 'myShortVec',
-        type: arrayTypeNode(publicKeyTypeNode(), prefixedCountNode(numberTypeNode('shortU16'))),
-    });
-
-    const options: GetRenderMapOptions = {
-        nameTransformers: DEFAULT_NAME_TRANSFORMERS,
-    };
-
-    // When we render the number.
-    const renderMap = visit(node, getRenderMapVisitor(options));
-    console.log(renderMap);
-
-    // Then we expect a short u16 to be exported.
-    // codeContains(getFromRenderMap(renderMap, 'types/my_short_u16.rs'), [
-    //     /pub type MyShortU16 = ShortU16/,
-    //     /use solana_short_vec::ShortU16/,
-    // ]);
-    // codeDoesNotContains(getFromRenderMap(renderMap, 'types/my_short_u16.rs'), [
-    //     /use borsh::BorshSerialize/,
-    //     /use borsh::BorshDeserialize/,
-    // ]);
+test('placeholder', () => {
+    expect(true).toBe(true);
 });
