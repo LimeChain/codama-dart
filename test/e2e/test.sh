a5bbae448b5c09491c1ecf7492c69a6456e6e274
ERoydev <e.roydev@gmail.com>
2025-12-18 09:25:36 +0200

    update: Improve e2e tests

--- test/e2e/test.sh
+++ test/e2e/test.sh
@@ -2,7 +2,7 @@
 set -eux
 
 function test_project() {
-    node test/e2e/generate.cjs $1 
+    node test/e2e/generate.cjs $1 true
     cd test/e2e/dart_generated_workspace/$1
 
     dart analyze
@@ -11,8 +11,26 @@ function test_project() {
 }
 
 function test_anchor_project() {
-    node test/e2e/generate-anchor.cjs $1
+    node test/e2e/generate-anchor.cjs $1 true
     cd test/e2e/dart_generated_workspace/$1
+
+    dart analyze
+
+    cd ../../../..
+}
+
+function test_anchor_without_workspace() {
+    node test/e2e/generate-anchor.cjs "$1"
+    cd test/e2e/dart_generated_workspace/${1}_generated
+
+    dart analyze
+
+    cd ../../../..
+}
+
+function test_project_without_workspace() {
+    node test/e2e/generate.cjs "$1"
+    cd test/e2e/dart_generated_workspace/${1}_generated
 
     dart analyze
 
@@ -24,3 +42,5 @@ test_project system
 test_project memo
 test_anchor_project anchor
 test_anchor_project data_structures
+test_project_without_workspace system
+test_anchor_without_workspace anchor
